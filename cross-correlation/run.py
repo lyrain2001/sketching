e8968538a4aedfaf357ac6469dee09164b2c5a79
lyrain2001 <yliu217@u.rochester.edu>
2023-11-26 03:59:06 -0500

    update cross correlation using scipy

--- cross-correlation/run.py
+++ cross-correlation/run.py
@@ -1,10 +1,15 @@
+import argparse
 import numpy as np
 import scipy.signal
 import time
 
 def generate_binary_data(size):
-    """Generate synthetic binary data."""
+    """Generate binary data."""
     return np.random.randint(0, 2, size)
+
+def generate_normal_data(size):
+    """Generate normally distributed data."""
+    return np.random.randn(size)
 
 def correlate_and_measure_time(data1, data2, method):
     """Perform cross-correlation and measure time."""
@@ -13,18 +18,28 @@ def correlate_and_measure_time(data1, data2, method):
     return time.time() - start_time
 
 def main():
-    # Generate synthetic binary data
-    size = 1000  # You can adjust this size
-    data1 = generate_binary_data(size)
-    data2 = generate_binary_data(size)
+    # Parsing command line arguments
+    parser = argparse.ArgumentParser(description="Cross-correlation efficiency analysis.")
+    parser.add_argument("--size", type=int, default=1000, help="Size of the synthetic data.")
+    parser.add_argument("--method", choices=['auto', 'direct', 'fft'], default='auto', help="Method for cross-correlation.")
+    parser.add_argument("--data_type", choices=['binary', 'normal'], default='binary', help="Type of synthetic data (binary or normal).")
 
-    # Methods to test
-    methods = ['direct', 'fft']
+    args = parser.parse_args()
 
-    # Measure and print time taken by each method
-    for method in methods:
-        time_taken = correlate_and_measure_time(data1, data2, method)
-        print(f"Method: {method}, Time Taken: {time_taken} seconds")
+    # Generate synthetic data
+    size = args.size
+    data_type = args.data_type
+    if data_type == 'binary':
+        data1 = generate_binary_data(size)
+        data2 = generate_binary_data(size)
+    else:
+        data1 = generate_normal_data(size)
+        data2 = generate_normal_data(size)
+
+    # Perform cross-correlation and measure time
+    method = args.method
+    time_taken = correlate_and_measure_time(data1, data2, method)
+    print(f"Method: {method}, Time Taken: {time_taken} seconds")
 
 if __name__ == "__main__":
     main()
